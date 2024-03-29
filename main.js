1c3cea80dae571e882ad0afd781c067bdeaffd03
AwaisArain <158747214+awais-student@users.noreply.github.com>
2024-03-30 00:54:02 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tWelcome to Awais-Arain CLI Number Guessing Game\n");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userguessingnumber",
+        type: "number",
+        message: "Enter your guessing number (Number Limit form (1 to 5) ) ",
+    },
+]);
+if (answer.userguessingnumber === randomNumber) {
+    console.log("Congratulation! You guess a correct number");
+}
+else {
+    console.log("Sorry! You guess a wrong number");
+}
